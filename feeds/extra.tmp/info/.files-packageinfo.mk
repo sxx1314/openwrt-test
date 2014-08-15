7fc1f43fe165585abe369aaddd4812073e78f14f
Sxx1314 <Sxx1314@hotmail.com>
2014-08-15 17:38:05 +0800

    code add

---
+++ feeds/extra.tmp/info/.files-packageinfo.mk
@@ -0,0 +1,6 @@
+$(eval $(call PackageDir,luci_luci-extra,luci/luci-extra))
+$(eval $(call PackageDir,packages_shadowsocks-libev,packages/shadowsocks-libev))
+$(eval $(call PackageDir,packages_webui-aria2,packages/webui-aria2))
+$(eval $(call PackageDir,packages_yaaw,packages/yaaw))
+$(eval $(call PackageDir,packages_dnscrypt-proxy,packages/dnscrypt-proxy))
+$(eval $(call PackageDir,libs_libsodium,libs/libsodium))
